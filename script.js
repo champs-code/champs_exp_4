39620de2e2af8c870a15aa6a323b65388886d5ec
Champi Brahma <u23cse1034@cit.ac.in>
2025-08-24 21:10:54 +0530

    js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const mainImage = document.getElementById("main-image");
+const thumbnails = document.querySelectorAll(".thumbnail");
+
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener("click", () => {
+    mainImage.src = thumbnail.src;
+    mainImage.alt = thumbnail.alt;
+  });
+});
